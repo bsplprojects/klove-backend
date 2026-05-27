d19bb4ae8c533e4f8e00b03fc0d50c4d11a06f87
Zeet <bsplzeet@gmail.com>
2026-05-27 22:22:16 +0530

    app.jsnew

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://backend.jbmglobal.pro",
+      "https://backend.jbmglobal.pro/",
      
     ],
     credentials: true,
